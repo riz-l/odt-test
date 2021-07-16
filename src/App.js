1f809029ec7cce9d3a9d3e710612c1b8f7b4881c
thericeisready <49308672+thericeisready@users.noreply.github.com>
2021-07-16 12:15:21 +0100

    Tried to break the repo

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import styled from "styled-components";
 
 // Import: Components
-import { Header } from "./app/components";
+// import { Header } from "./app/components";
 
 // Component: App
 export default function App() {
